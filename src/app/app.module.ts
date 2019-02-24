f5b84898a8058666d278650f9753e354e4c582be
lbanozic <lubanozic@gmail.com>
2019-02-24 10:25:12 +0100

    Add basic design for search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { MatExpansionModule, MatListModule, MatIconModule, MatButtonModule } from '@angular/material'
+import { MatExpansionModule, MatListModule, MatIconModule, MatButtonModule, MatInputModule, MatFormFieldModule } from '@angular/material'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,11 +14,14 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     BrowserAnimationsModule,
     MatExpansionModule,
     MatListModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
